26bff48321866a03c61120c67abe27b2fea7a87c
Ikiru Yoshizaki <cherishacquire@outlook.com>
2017-11-30 18:01:19 +0900

    change(async) async await

--- functions/src/addMessage.ts
+++ functions/src/addMessage.ts
@@ -7,8 +7,8 @@ export const addMessage = functions.https.onRequest(async (req, res) => {
   // Grab the text parameter.
   const original = req.query.text;
   // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  await db.ref('/messages').push({original: original}).then(snapshot => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    res.redirect(303, snapshot.ref);
-  });
+  const snapshot = await db.ref('/messages').push({original: original});
+
+  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+  res.redirect(303, snapshot.ref);
 });
